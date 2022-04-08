fc75f3ac5167751f21c37ccee3cbf0f54bff44f5
Thirstypooch <thirstypooch@protonmail.com>
2022-04-08 16:05:28 -0500

    jwt functionality

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,61 @@
+import NextAuth from 'next-auth'
+import SpotifyProvider from 'next-auth/providers/spotify'
+import spotifyApi, { LOGIN_URL } from '../../../lib/spotify.js'
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+    console.log("REFRESHED TOKEN IS ...", refreshedToken);
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000, //3600 is returned from spoti API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
+}
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientID: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
+    }),
+    // adding more providers
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // initial sign-in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refreshToken,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000, // in milliseconds
+        }
+      }
+      // return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('EXISTING ACCESS TOKEN IS VALID ! ')
+        return token
+      }
+      // access token is expired, up to refreshment
+      console.log('ACCESS TOKEN HAS EXPIRED, REFRESHING...')
+      return await refreshAccessToken(token)
+    },
+  },
+})
